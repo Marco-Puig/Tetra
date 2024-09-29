236185f0821004e4a65515ec27b17baf712f0d17
Marco Puig <90495366+Marco-Puig@users.noreply.github.com>
2024-09-29 13:45:25 -0400

    Finished Menu Animations

--- Assets/Scripts/Menu/PressStart.cs
+++ Assets/Scripts/Menu/PressStart.cs
@@ -1,15 +1,16 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Threading.Tasks;
+using TMPro;
 using UnityEngine;
 
 // script for when you press any key, you are transitioned to the main menu
 public class PressStart : MonoBehaviour
 {
+
+    [SerializeField] GameObject title; // title ui element
+    [SerializeField] GameObject cam; // move smoothly up to the main menu
     [SerializeField] GameObject[] showElements;
     [SerializeField] GameObject[] hideElements;
-    [SerializeField] GameObject cam; // move smoothly up to the main menu
+
     Action state;
     void Start()
     {
@@ -37,6 +38,7 @@ public class PressStart : MonoBehaviour
         foreach (GameObject element in showElements)
         {
             element.SetActive(true);
+            element.transform.position = Vector3.Lerp(element.transform.position, new Vector3(element.transform.position.x, 900f, element.transform.position.z), 1.25f * Time.deltaTime);
         }
         foreach (GameObject element in hideElements)
         {
@@ -45,7 +47,8 @@ public class PressStart : MonoBehaviour
 
         // move camera up to the main menu slowly and play ui animations
         // TODO: UI ANIMATIONS HERE
-        cam.transform.rotation = Quaternion.Lerp(cam.transform.rotation, Quaternion.Euler(10.0f, 0, 0), 1.25f * Time.deltaTime);
-        if (cam.transform.rotation.x == 10f) state = null; // once camera is at the right angle, stop the script
+        cam.transform.rotation = Quaternion.Lerp(cam.transform.rotation, Quaternion.Euler(17f, 0, 0), 1.25f * Time.deltaTime);
+        title.transform.position = Vector3.Lerp(title.transform.position, new Vector3(title.transform.position.x, 1450f, title.transform.position.z), 1.25f * Time.deltaTime);
+        if (cam.transform.rotation.x == 17f) state = null; // once camera is at the right angle, stop the script
     }
 }
