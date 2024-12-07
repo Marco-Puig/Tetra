654ea4fe96f865ea7b824abdfedad5fa8898a78b
Marco Puig <90495366+Marco-Puig@users.noreply.github.com>
2024-12-07 16:53:54 -0500

    Updated Docs for Swipe Detection

--- Assets/Scripts/MobileInput/SwipeDetection.cs
+++ Assets/Scripts/MobileInput/SwipeDetection.cs
@@ -8,6 +8,10 @@ public class Swipe_Controller : MonoBehaviour
 
     void Update()
     {
+		// Touch phase - Began: When the user first touches the screen
+		// Touch phase - Ended: When the user lifts their finger off the screen
+		// If the position of the touch position is different, then the user has swiped
+
 		// If touch is detected, store the start and end positions of the touch
 		// Check the touch state, so at the start of the touch state, the start position is stored
         if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
